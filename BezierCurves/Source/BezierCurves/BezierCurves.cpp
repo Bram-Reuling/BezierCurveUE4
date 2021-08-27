fa2c1822714dc3b5382fdee48434678b381d6a48
Bram Reuling <bram.reuling@outlook.com>
2021-08-27 17:46:53 +0200

    Initial project setup

---
+++ BezierCurves/Source/BezierCurves/BezierCurves.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BezierCurves.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BezierCurves, "BezierCurves" );
---
+++ BezierCurves/Source/BezierCurves/BezierCurvesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BezierCurvesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEZIERCURVES_API ABezierCurvesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
